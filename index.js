717720239b8e92fa5495650ac6859938b86a645d
Mohamed <bob@example.com>
2020-11-17 22:14:17 +0100

    cancel Typescript

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-import express from "express";
-import { config } from "dotenv";
+const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 const MongoClient = require("mongodb").MongoClient;
 const url = `mongodb://localhost:${port}/cheapshop`;
 
-export class Start {
-  constructor() {
-    if (config().error) {
-      throw config().error;
+    if (dotenv.error) {
+      throw dotenv.error;
     }
-    this.InitDB();
-    this.InitServer();
-    this.LunchServer();
-  }
 
-  InitDB() {
-      console.log("Init db success")
-  }
 
-  InitServer() {
     app.use(express.json());
     // app.use();
     app.get("/", (req, res) => {
       res.end("Hi tanguy are u okay");
     });
-  }
+  
 
-  LunchServer() {
     app.listen(port, () => {
       console.log("Application work fine");
     });
-  }
-}
-let start = new Start();
+  
+
